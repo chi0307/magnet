6be1296a0f1e0583bd08444c9d6581f474d20192
Junchi <benson26365156@gmail.com>
2024-08-18 15:31:07 +0800

    feat: add typia package and setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
+import UnpluginTypia from '@ryoppippi/unplugin-typia/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 import presetUno from 'unocss/preset-uno'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    UnpluginTypia(),
     UnoCSS({
-      presets: [
-        presetUno()
-      ]
+      presets: [presetUno()],
     }),
-    react()
+    react(),
   ],
 })
