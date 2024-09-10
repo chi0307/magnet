490349dbdc1d80cadb96571fde00ba04036d6689
Willis <willisxwu@gmail.com>
2024-09-10 00:24:12 -0400

    feat: update LedgerPage

--- src/pages/LedgerPage.tsx
+++ src/pages/LedgerPage.tsx
@@ -1,87 +1,157 @@
 import { FaShoppingCart } from 'react-icons/fa'
+import { IoAdd } from 'react-icons/io5'
+import { getCurrency } from '../utils/CurrencyManager'
+import { getLocale } from '../utils/locale'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import DoughnutPieChart from '../components/DoughnutPieChart'
+
 interface Transaction {
   date: string
-  items: { description: string; amount: string }[]
-  total: string
+  items: { description: string; amount: number }[]
+  total: number
 }
 
 const transactions: Transaction[] = [
   {
     date: '2024-08-01',
     items: [
-      { description: 'Groceries', amount: '$-50' },
-      { description: 'Coffee', amount: '$-5' },
+      { description: 'Groceries', amount: -50.98 },
+      { description: 'Coffee', amount: -5.03 },
     ],
-    total: '$-55',
+    total: -56.01,
   },
   {
     date: '2024-08-02',
     items: [
-      { description: 'Book', amount: '$-15' },
-      { description: 'Dinner', amount: '$-30' },
+      { description: 'Book', amount: -15 },
+      { description: 'Dinner', amount: -30 },
     ],
-    total: '$-45',
+    total: -45,
   },
   {
-    date: '2024-08-02',
+    date: '2024-08-05',
     items: [
-      { description: 'Book', amount: '$-15' },
-      { description: 'Dinner', amount: '$-30' },
+      { description: 'Book', amount: 15 },
+      { description: 'Dinner', amount: 30 },
     ],
-    total: '$-45',
+    total: 45,
   },
   {
-    date: '2024-08-02',
+    date: '2024-08-03',
     items: [
-      { description: 'Book', amount: '$-15' },
-      { description: 'Dinner', amount: '$-30' },
+      { description: 'Book', amount: -15 },
+      { description: 'Dinner', amount: -30 },
     ],
-    total: '$-45',
+    total: -45,
   },
 ]
 
 const LedgerPage = (): JSX.Element => {
+  const { t } = useTranslation()
+  const currency = getCurrency()
+  const locale = getLocale()
+
+  transactions.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  )
+
+  function formattedCurrency(dollar: number): string {
+    return new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency,
+    }).format(dollar)
+  }
+
   return (
     <div className='mx-auto px-4 pt-4 max-w-192 text-4.2'>
-      <div className='p-4 flex justify-between items-center text-bold-lg rounded-4 border border-gray-300'>
+      <div
+        className='
+          p-4
+          flex justify-between items-center
+          text-bold-lg rounded-4
+          border border-[#E5E5E5]
+        '
+      >
         <div className='text-left'>
-          <p className='text-gray-600 text-sm'>Expense</p>
-          <p className='text-red-500 text-xl'>$123,456</p>
+          <p className='text-[#4B4B4B] text-sm'>{t('ledger.expense')}</p>
+          <p className='text-[#FF4B4A] text-xl'>{formattedCurrency(123456)}</p>
         </div>
         <div className='text-right'>
-          <p className='text-gray-600 text-sm'>Income</p>
-          <p className='text-green-500 text-xl'>$0</p>
+          <p className='text-[#4B4B4B] text-sm'>{t('ledger.income')}</p>
+          <p className='text-[#1DB0F5] text-xl'>{formattedCurrency(654321)}</p>
         </div>
       </div>
-      <div className='mt-8 w-full h-30 bg-white rounded-4 border border-gray-300 flex flex-center'>
-        <div className='text-center text-gray-600 text-sm'>
-          我是圓餅圖，要裝 Chart.js
-        </div>
+      <div className='mx-auto my-8 w-80'>
+        <DoughnutPieChart />
       </div>
-      <div className='mt-8'>
+      <div>
         {transactions.map((transaction) => (
           <div key={transaction.date} className='mb-6 last:(mb-0 pb-4)'>
-            <div className='p-4 bg-gray-100 text-bold-md border border-gray-300 rounded-4'>
-              <div className='px-3 pb-3 flex justify-between items-center text-(5 bold-lg) border-b border-gray-300'>
-                <span className='text-gray-800'>{transaction.date}</span>
-                <span className='text-emerald-600'>{transaction.total}</span>
+            <div className='p-4 bg-[#F6F4EF] text-bold-md rounded-4'>
+              <div
+                className='
+                  px-3 pb-3
+                  flex justify-between items-center
+                  border-b border-[#E5E5E5]
+                  '
+              >
+                <span className='text-[#4B4B4B]'>{transaction.date}</span>
+                <span
+                  className={`
+                    text-bold-lg 
+                    ${
+                      transaction.total > 0
+                        ? 'text-[#1DB0F5]'
+                        : 'text-[#FF4B4A]'
+                    }
+                  `}
+                >
+                  {formattedCurrency(transaction.total)}
+                </span>
               </div>
               {transaction.items.map((item, index) => (
                 <div
                   key={index}
-                  className='px-3 py-2 flex justify-between items-center border-b border-gray-300 last:(pb-0 border-none)'
+                  className='
+                    px-3 py-2
+                    flex justify-between items-center
+                    border-b border-[#E5E5E5]
+                    last:(pb-0 border-none)
+                  '
                 >
-                  <FaShoppingCart className='text-gray-500 min-w-6 min-h-6' />
-                  <span className='ml-3 mr-2 w-full text-ellipsis line-clamp-1 text-gray-700'>
+                  <FaShoppingCart className='min-w-6 min-h-6 text-[#4B4B4B]' />
+                  <span
+                    className='
+                      ml-3 mr-2
+                      flex-1
+                      text-[#4B4B4B] text-ellipsis line-clamp-1
+                    '
+                  >
                     {item.description}
                   </span>
-                  <span className='text-gray-900'>{item.amount}</span>
+                  <span className='text-[#4B4B4B]'>
+                    {formattedCurrency(item.amount)}
+                  </span>
                 </div>
               ))}
             </div>
           </div>
         ))}
       </div>
+      <Link
+        to={'/ledger/add'}
+        className='
+          fixed bottom-5% left-1/2
+          flex flex-center
+          w-15 h-15
+          transform -translate-x-1/2
+          bg-[#FEC700] rounded-full
+          border-2 border-white
+        '
+      >
+        <IoAdd className='w-10 h-10 text-white' />
+      </Link>
     </div>
   )
 }
