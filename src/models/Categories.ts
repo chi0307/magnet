3589f0e88c4bc9450e9d869f32ba36e361aa1409
Junchi <benson26365156@gmail.com>
2024-09-12 17:34:47 +0800

    feat: modify categories findAll method

--- src/models/Categories.ts
+++ src/models/Categories.ts
@@ -1,7 +1,15 @@
+import { type CategoryInfoEntity } from '../types/database'
+import { type UUID } from '../types/utils'
 import { BaseTable } from './BaseTable'
 
 export class CategoryTable extends BaseTable<'categoryInfo'> {
-  public constructor() {
+  private _ledgerId: UUID
+  public constructor(ledgerId: UUID) {
     super('categoryInfo')
+    this._ledgerId = ledgerId
+  }
+
+  public override async findAll(): Promise<CategoryInfoEntity[]> {
+    return this.table.where({ ledgerId: this._ledgerId }).toArray()
   }
 }
