3acd92c22d7bbc34202880894b23084bb498410d
Junchi <benson26365156@gmail.com>
2024-09-12 17:44:07 +0800

    feat: add findAll categories sorting

--- src/models/Categories.ts
+++ src/models/Categories.ts
@@ -9,7 +9,19 @@ export class CategoryTable extends BaseTable<'categoryInfo'> {
     this._ledgerId = ledgerId
   }
 
+  /**
+   * sorting:
+   * 1. sort_index big -> small
+   * 2. created_at old -> new
+   */
   public override async findAll(): Promise<CategoryInfoEntity[]> {
-    return this.table.where({ ledgerId: this._ledgerId }).toArray()
+    const categories = await this.table
+      .where({ ledgerId: this._ledgerId })
+      .toArray()
+    return categories.sort(
+      (aItem, bItem) =>
+        bItem.sortIndex - aItem.sortIndex ||
+        (aItem.createdAt > bItem.createdAt ? 1 : -1)
+    )
   }
 }
