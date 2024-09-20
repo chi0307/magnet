3df73753fdb529ba4869cd82a61e60e8460ca72c
Willis <willisxwu@gmail.com>
2024-09-20 19:36:47 -0400

    feat: add onClick prop at SignInButton

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-import { localStorageManager } from '@/utils/StorageManager'
-
 interface SignInButtonProps {
   color: string
   icon: React.ReactNode
   text: string
+  onClick: () => void
   className?: string
 }
 
@@ -13,18 +12,14 @@ const SignInButton = ({
   color,
   icon,
   text,
+  onClick,
   className = '',
 }: SignInButtonProps): JSX.Element => {
-  const handleClick = (method: string): void => {
-    localStorageManager.set('loginMethod', method)
-    window.location.reload()
-  }
-
   return (
     <button
       className={`px-6 py-2 min-w-40 rounded-5 text-white text-bold-lg ${className}`}
       style={{ backgroundColor: color }}
-      onClick={() => handleClick(text)}
+      onClick={onClick}
     >
       <div className='mx-auto flex flex-center gap-2'>
         <div>{icon}</div>
