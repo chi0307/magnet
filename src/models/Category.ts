6140dd19a0bd77a257545de5f7a3f20e23999a4c
Junchi <benson26365156@gmail.com>
2024-09-25 09:30:19 +0800

    remove: Category class business logic

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,29 +1,8 @@
 import { BaseTable } from '@/models/BaseTable'
 import { type CategoryEntity } from '@/types/database'
-import { type UUID } from '@/types/utils'
 
 export class Category extends BaseTable<CategoryEntity> {
-  private ledgerId: UUID
-  public constructor(ledgerId: UUID) {
+  public constructor() {
     super('category')
-    this.ledgerId = ledgerId
-  }
-
-  /**
-   * sorting:
-   * 1. sort_index big -> small
-   * 2. created_at old -> new
-   */
-  public override async findAll(): Promise<
-    readonly Readonly<CategoryEntity>[]
-  > {
-    const categories = await this.table
-      .filter((item) => item.ledgerId === this.ledgerId)
-      .toArray()
-    return categories.sort(
-      (aItem, bItem) =>
-        bItem.sortIndex - aItem.sortIndex ||
-        (aItem.createdAt > bItem.createdAt ? 1 : -1)
-    )
   }
 }
