fd0d6dab8bfca97b32bd724a7b36c0b01b6c431a
Junchi <benson26365156@gmail.com>
2024-10-08 16:48:51 +0800

    rename: `ExcludeAutoBaseEntity` to `RequiredEntity`

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -5,7 +5,7 @@ export type UUID = `${string}-${string}-${string}-${string}-${string}`
 export type TableName<Entity extends AutoBaseEntity, TableSchema> = {
   [K in keyof TableSchema]: TableSchema[K] extends Entity ? K : never
 }[keyof TableSchema]
-export type ExcludeAutoBaseEntity<Entity extends AutoBaseEntity> = Omit<
+export type RequiredEntity<Entity extends AutoBaseEntity> = Omit<
   Entity,
   keyof AutoBaseEntity
 >
