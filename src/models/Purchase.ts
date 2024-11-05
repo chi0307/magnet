726890cf1aaa86ac2c30bd6d7ac441206180e210
Junchi <benson26365156@gmail.com>
2024-11-05 15:46:20 +0800

    feat: purchase model add find by ledger id

--- src/models/Purchase.ts
+++ src/models/Purchase.ts
@@ -5,4 +5,11 @@ export class Purchase extends BaseTable<PurchaseEntity> {
   public constructor() {
     super('purchase')
   }
+
+  // TODO: 這邊要想一下 find 有沒有需要可以透過名稱馬上知道是取得一個還是 array
+  public async findByLedgerId(
+    ledgerId: PurchaseEntity['ledgerId']
+  ): Promise<readonly Readonly<PurchaseEntity>[]> {
+    return this.table.where({ ledgerId }).toArray()
+  }
 }
