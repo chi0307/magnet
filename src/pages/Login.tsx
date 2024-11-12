4aa3cd98a005d32d40cea5de1a1cae10db466533
Willis <willisxwu@gmail.com>
2024-11-11 21:01:01 -0500

    refactor: rename SignInPage to Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,15 +3,15 @@ import { useTranslation } from 'react-i18next'
 import { FaFacebookF, FaGoogle, FaApple, FaUser, FaLine } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 
-import SignInButton from '@/components/SignInButton'
+import LoginButton from '@/components/LoginButton'
 import { signInOrRegisterUser } from '@/services/User'
 import { initializeCurrency } from '@/utils/CurrencyManager'
 import { localStorageManager } from '@/utils/StorageManager'
 
-const SignInPage = (): JSX.Element => {
+const Login = (): JSX.Element => {
   const { t } = useTranslation()
   const navigate = useNavigate()
-  const buttons: Omit<Parameters<typeof SignInButton>[0], 'onClick'>[] = [
+  const buttons: Omit<Parameters<typeof LoginButton>[0], 'onClick'>[] = [
     { color: '#4267B2', icon: <FaFacebookF />, text: 'facebook' },
     { color: '#DB4437', icon: <FaGoogle />, text: 'google' },
     { color: '#4BC764', icon: <FaLine />, text: 'line' },
@@ -19,7 +19,7 @@ const SignInPage = (): JSX.Element => {
   ]
 
   const handleRegisterUser = async (service: string): Promise<void> => {
-    console.info('logging service is ', service)
+    console.info('service: ', service)
 
     // mock sso user data
     const userData = {
@@ -30,7 +30,7 @@ const SignInPage = (): JSX.Element => {
     try {
       const userId = await signInOrRegisterUser(userData)
       localStorageManager.set('userId', userId)
-      navigate('/magnet/ledger')
+      navigate('/magnet/book')
     } catch {
       console.error('register fail')
     }
@@ -42,15 +42,17 @@ const SignInPage = (): JSX.Element => {
 
   return (
     <div className='mx-auto max-w-107.5 max-h-100dvh flex items-center flex-col'>
-      <p className='mt-15 px-5 text-(8 bold-lg)'>{t('general.welcome')}</p>
+      <p className='mt-15 px-5 text-(8 bold-lg)'>
+        {t('general.application_name')}
+      </p>
       <img src='https://fakeimg.pl/400/' alt='Logo' className='mt-8 max-w-50' />
       <div className='mt-20 flex flex-center flex-col gap-4'>
         {buttons.map((button, index) => (
-          <SignInButton
+          <LoginButton
             key={index}
             color={button.color}
             icon={button.icon}
-            text={t(`sign-in.${button.text}`)}
+            text={t(`login.${button.text}`)}
             onClick={() => {
               handleRegisterUser(button.text).catch((error) => {
                 console.error('Error during user registration:', error)
@@ -59,10 +61,10 @@ const SignInPage = (): JSX.Element => {
           />
         ))}
         <span className='text-4'>-</span>
-        <SignInButton
+        <LoginButton
           color='#9E9E9E'
           icon={<FaUser />}
-          text={t('sign-in.guest')}
+          text={t('login.guest')}
           onClick={() => {
             handleRegisterUser('guest').catch((error) => {
               console.error('Error during user registration:', error)
@@ -74,4 +76,4 @@ const SignInPage = (): JSX.Element => {
   )
 }
 
-export default SignInPage
+export default Login
