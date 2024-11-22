13ad0cede5d41ab44874141f25e0fdcffc29c1d2
Junchi <benson26365156@gmail.com>
2024-11-22 14:09:48 +0800

    feat: add error event

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,3 +14,24 @@ export function generateUuid(): UUID {
 export function uniqueArray<T>(array: T[]): T[] {
   return Array.from(new Set(array))
 }
+
+export function errorEvent(
+  message: string,
+  {
+    error = null,
+    type = 'console.error'
+  }: {
+    error?: unknown
+    type?: 'console.error' | 'alert'
+  } = {}
+): void {
+  const event: (text: string) => void = type === 'console.error' ? console.error : alert
+  if (error === null) {
+    event(message)
+  } else if (error instanceof Error) {
+    event(`${message}, error: ${error.message}`)
+  } else {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    event(`${message}, unknown: ${error}`)
+  }
+}
