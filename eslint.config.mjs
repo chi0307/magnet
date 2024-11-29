6e46324677479c0c87ee8d8365b35810bcd59f0a
Junchi <benson26365156@gmail.com>
2024-11-29 23:49:53 +0800

    build: fix 'perfectionist/sort-imports' role pattern

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -54,7 +54,7 @@ export default tsEslint.config(
             'object',
             'unknown',
           ],
-          internalPattern: ['@/**'],
+          internalPattern: ['^@/.*'],
         },
       ],
     },
